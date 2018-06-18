5359e9effc0f12acd19f09b9ea3b77c2a34030a1
Yvonne <ennovysun0629@gmail.com>
2018-06-18 13:13:13 +0800

    mappinp bug fixed

--- app/src/main/java/com/yvonne/alumnicardfunctions/MainActivity.java
+++ app/src/main/java/com/yvonne/alumnicardfunctions/MainActivity.java
@@ -3,6 +3,9 @@ package com.yvonne.alumnicardfunctions;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,11 +20,52 @@ public class MainActivity extends AppCompatActivity {
     public static Boolean mLocationPermissionGranted = false;
     public static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            string a = "";
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            string a = "";
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            string a = "";
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            string a = "";
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getLocationPermission();
+
+        LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == (PackageManager.PERMISSION_GRANTED)) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == (PackageManager.PERMISSION_GRANTED)) {
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10,
+                        10, mLocationListener);
+                mLocationPermissionGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+
+
     }
 
     public void getLocationPermission(){
@@ -67,4 +111,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,ListActivity.class);
         startActivity(intent);
     }
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/yvonne/alumnicardfunctions/SettingLocationActivity.java
+++ app/src/main/java/com/yvonne/alumnicardfunctions/SettingLocationActivity.java
@@ -1,17 +1,24 @@
 package com.yvonne.alumnicardfunctions;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,16 +28,22 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SettingLocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private LocationManager locationManager;
     private FusedLocationProviderClient mfusedLocationProviderClient;
     private static final float DEFAULT_ZOOM = 15f;
+    private EditText mSearchText;
+    private static final String TAG = "SettingLocationActivity";
 
 
     @Override
@@ -40,36 +53,78 @@ public class SettingLocationActivity extends FragmentActivity implements OnMapRe
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        getDeviceLocation();
+        mSearchText = (EditText)findViewById(R.id.location_setting_field);
 
     }
 
+    private void init(){
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        ||actionId == EditorInfo.IME_ACTION_DONE
+                        ||event.getAction() == KeyEvent.ACTION_DOWN
+                        ||event.getAction() == KeyEvent.KEYCODE_ENTER){
+                    //execute search method
+                    geoLocate();
 
-    private void getDeviceLocation(){
-        mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        try{
-            Task location = mfusedLocationProviderClient.getLastLocation();
-            location.addOnCompleteListener(new OnCompleteListener() {
-                @Override
-                public void onComplete(@NonNull Task task) {
-                    if(task.isSuccessful()&&task.getResult() != null){
-                    Location currentLocation = (Location)task.getResult();
-                    moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),DEFAULT_ZOOM);
-                    }else{
-                        Toast.makeText(SettingLocationActivity.this,"unable to get location",Toast.LENGTH_SHORT).show();
-                    }
                 }
-            });
+                return false;
+            }
+        });
+        hideSoftKeyboard();
+    }
 
-        }catch (SecurityException e){
+    private void geoLocate(){
+        String searchString = mSearchText.getText().toString();
+        Geocoder geocoder = new Geocoder(SettingLocationActivity.this);
+        List<Address> list = new ArrayList<>();
+        try {
+            list = geocoder.getFromLocationName(searchString,1);
+        }catch (IOException e){
+            Log.e(TAG,"geoLocate: IOException" + e.getMessage());
+        }
+
+        if(list.size()>0){
+            Address address = list.get(0);
+            Log.d(TAG,"geoLocate: found a location" + address.toString());
+            moveCamera(new LatLng(address.getLatitude(),address.getLongitude()),DEFAULT_ZOOM,address.getAddressLine(0));
+        }
+
+    }
+
+    private void getDeviceLocation() {
+        mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        try {
+            if (MainActivity.mLocationPermissionGranted) {
+                Task location = mfusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            Location currentLocation = (Location) task.getResult();
+                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM,"My Location");
+                        } else {
+                            Toast.makeText(SettingLocationActivity.this, "unable to get location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
 
         }
 
     }
 
-    private void moveCamera(LatLng latLng,float zoom){
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+    private void moveCamera(LatLng latLng, float zoom, String title) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        if(!title.equals("My Location")) {
+            MarkerOptions options = new MarkerOptions().position(latLng).title(title);
+            mMap.addMarker(options);
+        }
+        hideSoftKeyboard();
     }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -83,11 +138,17 @@ public class SettingLocationActivity extends FragmentActivity implements OnMapRe
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        //getDeviceLocation();
+        if (MainActivity.mLocationPermissionGranted) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            getDeviceLocation();
+
+        }
     }
 
     public void search(View view){
@@ -98,6 +159,9 @@ public class SettingLocationActivity extends FragmentActivity implements OnMapRe
 
     }
 
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
 
 
 }
